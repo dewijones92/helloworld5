69b98d8af66395e929d4fec8181279ee76d2c832
Dewi Jones <dewijones92@gmail.com>
2022-07-27 11:04:15 +0100

    more

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
 module Lib
-    ( someFunc
+    ( someFunc,
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+ putStrLn "asdasd"
+ let foo = undefined
+ print $ product [1..100]
+ print $ dewi20 [1..100]
+ print $ f44 ()
+ print $ fInt 2
+
+dewi20 :: ( Num a ) => [a] -> [a]
+dewi20 = map (+1) . reverse
+
+f44 :: () -> Integer
+f44 () = 44
+ 
+fInt :: Integer -> ()
+fInt _ = ()
+
+class DMonoid m where
+  dmempty :: m
+  dmappend :: m -> m -> m
+
+instance DMonoid String where
+  dmempty = ""
+  dmappend = (++)
