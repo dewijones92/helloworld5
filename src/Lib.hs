42fd7f27f1e7e4c5f1772f11ff89cb5a9c5de4d3
Dewi Jones <dewijones92@gmail.com>
2022-07-27 11:19:25 +0100

    works

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,25 +5,14 @@ module Lib
 someFunc :: IO ()
 someFunc = do
  putStrLn "asdasd"
- let foo = undefined
- print $ product [1..100]
- print $ dewi20 [1..100]
- print $ f44 ()
- print $ fInt 2
 
-dewi20 :: ( Num a ) => [a] -> [a]
-dewi20 = map (+1) . reverse
 
-f44 :: () -> Integer
-f44 () = 44
- 
-fInt :: Integer -> ()
-fInt _ = ()
 
-class DMonoid m where
-  dmempty :: m
-  dmappend :: m -> m -> m
+class DFunctor f where
+  dfmap :: (a -> b) -> f a -> f b
 
-instance DMonoid String where
-  dmempty = ""
-  dmappend = (++)
+
+instance DFunctor IO where
+  dfmap f action = do
+    result <- action
+    return (f result)
