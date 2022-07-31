651e2993ed476f72e824243cbe2d9c236b0d1040
Dewi Jones <dewijones92@gmail.com>
2022-07-31 10:42:31 +0100

    more

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,13 +20,25 @@ hmmm2 = do
           print $ (*3) . (+100) $ 1
           let ggg =  dfmap (replicate 3) (Left "foo")
           print $ dfmap (*2) (Just 2)
-          let dewi1 = fmap (++) (Just "hey")
-          let lol = Just (++ "aas")
+
+          let dewi22 = fmap (*) [1,2,3,4]
+          let dewres = fmap (\f -> f 9) dewi22
+
+
           putStrLn "asd"
+
+data Maybe2 a = Just2 a | Nothing2 deriving Show
+
+
+instance Functor Maybe2 where
+  fmap f (Just2 a) = Just2 (f a)
+  fmap f Nothing2 = Nothing2
 
 instance DFunctor CMaybe where
   dfmap f CNothing = CNothing
   dfmap f (CJust counter x) = CJust (counter + 1) (f x)
+
+data Tree a = Tip a | Branch (Tree a) (Tree a)
 
 
 data CMaybe a = CNothing | CJust Int a deriving (Show)
