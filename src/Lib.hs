a6bc2171a925cccc72e8db441cf6e056d6255ac9
Dewi Jones <dewijones92@gmail.com>
2022-08-14 08:13:25 +0100

    more

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,80 +1,35 @@
 module Lib
-    ( someFunc 
+    ( someFunc
     ) where
 import Data.List (intersperse)
 import Data.Char
 
 someFunc :: IO ()
 someFunc = do
-  hmmm2
-
-hmm1 :: IO ()
-hmm1 = do line <- getLine   
-          let line' = reverse line  
-          putStrLn $ "You said " ++ line' ++ " backwards!"  
-          putStrLn $ "Yes, you really said" ++ line' ++ " backwards!"  
-
-hmmm2 = do 
-          print $ dfmap (*3) (+100) 1
-          print $ (+3) `dfmap` (+4) $ 1
-          print $ (*3) . (+100) $ 1
-          let ggg =  dfmap (replicate 3) (Left "foo")
-          print $ dfmap (*2) (Just 2)
-
-          let dewi22 = fmap (*) [1,2,3,4]
-          let dewres = fmap (\f -> f 9) dewi22
-
-          let x = Branch (Tip 4) (Branch (Tip 5) (Tip 6))
-          print x
-          let xx = (*) <$> x
+  print $ show $ Just2 3
+  print "heyyy"
+  print $ show $ dfmap id (Just2 3)
+  putStrLn "hey11"
+  print $ show $ id (Just2 3)
+  putStrLn "hey22"
+  print $ dfmap (+3) [1..10]
+  putStrLn "hey33"
+  let ff = fmap (+3) (+3)
+  putStr "asdasd"
 
 
-          putStrLn "asd"
-
-data Maybe2 a = Just2 a | Nothing2 deriving Show
 
 
-instance Functor Maybe2 where
-  fmap f (Just2 a) = Just2 (f a)
-  fmap f Nothing2 = Nothing2
 
-instance Applicative Maybe2 where
-  pure = Just2
+data Maybe2 a = Nothing2 | Just2 a deriving Show
 
-instance DFunctor CMaybe where
-  dfmap f CNothing = CNothing
-  dfmap f (CJust counter x) = CJust (counter + 1) (f x)
-
-data Tree a = Tip a | Branch (Tree a) (Tree a) deriving Show
-
-instance Functor Tree where
-  fmap f (Tip a) = Tip (f a)
-  fmap f (Branch left right) = Branch (fmap f left) (fmap f right)
-
-data CMaybe a = CNothing | CJust Int a deriving (Show)
-           
-
-dewiadd :: (Num a) => a -> a -> a
-dewiadd = (+)
-
-instance DFunctor (Maybe) where
-  dfmap f (Just a) = Just (f a)
-  dfmap _ Nothing  = Nothing
-
-instance DFunctor ( Either a) where
-  dfmap f (Right a) = Right (f a)
-  dfmap f (Left a) = Left a
-
-instance DFunctor ((->) r) where
-  dfmap = (.)
-  -- dfmap = (.)
-
-
-class DFunctor f where
+class Functor2 f where
   dfmap :: (a -> b) -> f a -> f b
 
+instance Functor2 [] where
+  dfmap = map
 
-instance DFunctor IO where
-  dfmap f action = do
-    result <- action
-    return (f result)
+instance Functor2 Maybe2 where
+  dfmap f Nothing2 = Nothing2
+  dfmap f (Just2 x) = Just2 (f x)
+
